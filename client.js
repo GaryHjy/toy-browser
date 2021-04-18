36fdd9a08647d0977f789274ec3d989db172137f
GaryHjy <782316959@qq.com>
2021-04-19 01:11:00 +0800

    feat: 完成ResponseParser及TrunkedBodyParser

--- client.js
+++ client.js
@@ -23,6 +23,7 @@ class Request {
 
   send(connection) {
     return new Promise((resolve, reject) => {
+      const parser = new ResponseParser;
       if (connection) {
         connection.write(this.toString());
       } else {
@@ -35,7 +36,11 @@ class Request {
       }
   
       connection.on('data', function(data) {
-        resolve(data.toString());
+        console.log(data.toString());
+        parser.receive(data.toString());
+        if (parser.isFinished) {
+          resolve(parser.response);
+        }
         connection.end();
       })
 
@@ -55,6 +60,165 @@ ${this.bodyText}
 };
 
 class Response {};
+
+// HTTP/1.1 200 OK                        ---------- statusLine
+// Content-Type: text/html                ---------- headers
+// Date: Mon, 23 Dec 2019 06:46:19 GMT
+// Connection: keep-alive
+// Transfer-Encoding: chunked             ---------- headers end
+// 
+// 26                                     ---------- body
+// <html><body>Hello Word</body></html>
+//                                        ---------- 换行
+// 
+// 0                                      ---------- body end
+//                                        ---------- 换行
+class ResponseParser {
+  constructor() {
+    this.WAITING_STATUS_LINE = 0;
+    this.WAITING_STATUS_LINE_END = 1;
+    this.WAITING_HEADER_NAME = 2;
+    this.WAITING_HEADER_SPACE = 3;
+    this.WAITING_HEADER_VALUE = 4;
+    this.WAITING_HEADER_LINE_END = 5;
+    this.WAITING_HEADER_BLOCK_END = 6;
+    this.WAITING_BODY = 7;
+
+    this.current = this.WAITING_STATUS_LINE;
+    this.statusLine = '';
+    this.headers = {};
+    this.headerName = '';
+    this.headerValue = '';
+    this.bodyParser = null;
+  }
+
+  get isFinished() {
+    return this.bodyParser && this.bodyParser.isFinished;
+  }
+
+  get response() {
+    this.statusLine.match(/HTTP\/1.1 ([0-9]+) ([\s\S]+)/);
+    return {
+      statusCode: RegExp.$1,
+      statusText: RegExp.$2,
+      headers: this.headers,
+      body: this.bodyParser.content.join('')
+    }
+  }
+
+  receive(string) {
+    for (let i = 0; i < string.length; i++) {
+      this.receiveChar(string.charAt(i))
+    }
+  }
+
+  receiveChar(char) {
+    // 判断是否为statueLine 追加到statusLine字符中
+    if (this.current === this.WAITING_STATUS_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_STATUS_LINE_END;
+      // } else if (char === '\n') {
+      //   console.log('n');
+      //   this.current = this.WAITING_HEADER_NAME;
+      } else {
+        this.statusLine += char;
+      }
+    }
+    else if (this.current === this.WAITING_STATUS_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    }
+    else if (this.current === this.WAITING_HEADER_NAME) {
+      if (char === ':') {
+        this.current = this.WAITING_HEADER_SPACE;
+      } else if (char === '\r') {
+        this.current = this.WAITING_HEADER_BLOCK_END;
+        if (this.headers['Transfer-Encoding'] === 'chunked') {
+          this.bodyParser = new TrunkedBodyParser();
+        }
+      } else {
+        this.headerName += char;
+      }
+    }
+    else if (this.current === this.WAITING_HEADER_SPACE) {
+      if (char === ' ') {
+        this.current = this.WAITING_HEADER_VALUE;
+      }
+    }
+    else if (this.current === this.WAITING_HEADER_VALUE) {
+      if (char === '\r') {
+        this.current = this.WAITING_HEADER_LINE_END;
+        this.headers[this.headerName] = this.headerValue;
+        this.headerName = '';
+        this.headerValue = '';
+      } else {
+        this.headerValue += char;
+      }
+    }
+    else if (this.current === this.WAITING_HEADER_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_HEADER_NAME;
+      }
+    }
+    else if (this.current === this.WAITING_HEADER_BLOCK_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_BODY;
+      }
+    }
+    else if (this.current === this.WAITING_BODY) {
+      this.bodyParser.receiveChar(char);
+    }
+  }
+};
+
+class TrunkedBodyParser {
+  constructor() {
+    this.WAITING_LENGTH = 0;
+    this.WAITING_LENGTH_LINE_END = 1;
+    this.READING_TRUNK = 2;
+    this.WAITING_NEW_LINE = 3;
+    this.WAITING_NEW_LINE_END = 4;
+
+    this.length = 0;
+    this.content = [];
+    this.isFinished = false;
+    
+    this.current = this.WAITING_LENGTH;
+  }
+
+  receiveChar(char) {
+    if (this.current === this.WAITING_LENGTH) {
+      if (char === '\r') {
+        this.current = this.WAITING_LENGTH_LINE_END;
+        if (this.length === 0) {
+          this.isFinished = true;
+        }
+      } else {
+        this.length *= 10;
+        this.length += char.charCodeAt(0) - '0'.charCodeAt(0);
+      }
+    } else if (this.current === this.WAITING_LENGTH_LINE_END) {
+      if (char === '\n') {
+        this.current = this.READING_TRUNK;
+      }
+    } else if (this.current === this.READING_TRUNK) {
+      this.content.push(char);
+      this.length--;
+      if (this.length === 0) {
+        this.current = this.WAITING_NEW_LINE;
+      }
+    } else if (this.current === this.WAITING_NEW_LINE) {
+      if (char === '\r') {
+        this.current = this.WAITING_NEW_LINE_END;
+      }
+    } else if (this.current === this.WAITING_NEW_LINE_END) {
+      if (char === '\n') {
+        this.current = this.WAITING_LENGTH;
+      }
+    }
+  }
+}
 
 // const client = net.createConnection({
 //   host: 'localhost',
