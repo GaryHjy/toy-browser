e1adfd88fc87e2381345b27b193f0e6bcec199c5
GaryHjy <782316959@qq.com>
2021-05-26 23:55:24 +0800

    feat: create tag

--- parser.js
+++ parser.js
@@ -1,17 +1,34 @@
 const EOF = Symbol("EOF"); // EOF: End Of File
 
+let currentToken = null;
+
+function emit(token) {
+  console.log(token);
+}
+
 function data(c) {
   if (c === "<") {
     return tagOpen;
   } else if (c === EOF) {
+    emit({
+      type: "EOF"
+    })
     return;
   } else {
+    emit({
+      type: "text",
+      content: c
+    })
     return data;
   }
 }
 
 function tagOpen(c) {
   if (c.match(/^[a-zA-Z]$/)) {
+    currentToken = {
+      type: "startTag",
+      tagName: ""
+    }
     return tagName(c);
   } else if (c === "/") {
     return endTagOpen;
@@ -32,15 +49,18 @@ function endTagOpen(c) {
 
 function tagName(c) {
   if (c.match(/^[a-zA-Z]$/)) {
+    // 拼接tagName
+    currentToken.tagName += c;
     // 匹配字母代表为标签名
     return tagName;
-  } else if (c.match(/^[\t\n\t ]$/)) {
+  } else if (c.match(/^[\t\n\f ]$/)) {
     // 匹配空格代表属性
     return beforeAttributeName;
   } else if (c === "/") {
     // 代表标签结束
     return selfClosingStartTag;
   } else if (c === ">") {
+    emit(currentToken);
     return data;
   } else {
     return tagName;
@@ -61,9 +81,10 @@ function beforeAttributeName(c) {
 
 function selfClosingStartTag(c) {
   if (c === ">") {
+    // 自封闭标签
+    currentToken.isSelfClosing = true;
     return data;
   } else if (c === "EOF") {
-
   } else {
 
   }
